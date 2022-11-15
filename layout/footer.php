3f447e39bca945c78d1a19b1f960244d4a3f4fb6
Duncan Phelps <phelps.duncan@gmail.com>
2022-11-15 15:47:27 -0700

    finished course/episode 1

---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <p>&copy; AirPupnMeow.com</p>
+</footer>
+</div>
+<!-- /container -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
